6424003d6a864b39051bd785da1170eef3709181
Balázs Vécsei <balazsvecsei9@gmail.com>
2025-01-07 13:08:17 +0100

    scroll animáció

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+const primaryColor = new THREE.Color(0x050057)
 
 const canvasDIV = document.querySelector('#bg');
 const scene = new THREE.Scene();
- scene.background = new THREE.Color(0xffffff)
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvasDIV
+    canvas: canvasDIV,
+    antialias: true
 })
+
+var scrollPosition = 0
+var model
 
 renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-camera.position.setZ(40);
+camera.position.setZ(30);
 
 
-const dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+const dirLight = new THREE.DirectionalLight( 0xffffff, 9 );
 dirLight.position.set( 0, 20, 10 );
 scene.add( dirLight );
 
@@ -25,7 +29,7 @@ scene.add( dirLight );
 
 const pointlight = new THREE.PointLight(0x9998f9, 200)
 
-pointlight.position.set(0,-10,10)
+pointlight.position.set(0,1,14)
 
 scene.add(pointlight  )
 
@@ -33,23 +37,24 @@ const loader = new GLTFLoader();
 
 const materialGlass = new THREE.MeshPhysicalMaterial( {
     transparent: true,
-    transmission: 1,
-    metalness: 0.47,
-    roughness: 0.2,
-    ior: 1.5,
-    thickness: 0.7,
+    transmission: .5,
+    metalness: 2,
+    roughness: 0.1,
+    ior: 1,
+    thickness: .2,
     specularIntensity: 1,
     specularColor: 0xffffff,
-    side: THREE.FrontSide
+    side: THREE.FrontSide,
+    color: primaryColor
 
   } );
 
   const material2 = new THREE.MeshPhysicalMaterial({
     metalness: 1,
-    roughness: 0.4,
+    roughness: 0.3,
     transparent: false,
-    transmission: 1.0,
-    side: THREE.FrontSide
+    transmission: 1.4,
+    color: 0x7A7AED
 });
 
 loader.load( '/minta-modern-export.gltf', function ( gltf ) {
@@ -57,37 +62,49 @@ loader.load( '/minta-modern-export.gltf', function ( gltf ) {
 
     let csikok = root.getObjectByName('csíkok')
     let savok = root.getObjectByName('sávok')
+    let kitoltes = root.getObjectByName('kitöltés')
+
+    kitoltes.material = material2
 
     savok.material = csikok.material = materialGlass
 
-    root.rotation.x = Math.PI / 3
+    root.rotation.x = Math.PI / 3 
     root.position.y = 5
 
-    scene.add( root )
+    model = root
+    scene.add(model)
+
 }, undefined, ( error ) => console.error( error ))
+
 
 const interaction = () => {
     requestAnimationFrame(interaction)
-
-    const mousemove = (e) => {
-
-
-        const client = (e.touches) ? e.touches[0] : e;
-
-        pointlight.position
-            .setX(
-                (client.clientX - window.innerWidth / 2) /10 * 0.55
-            )
-            .setZ(
-                (client.clientY - window.innerHeight / 2) /10 * .55
-            )
-
-    }
-
-    document.addEventListener("mousemove", mousemove)
-    document.addEventListener("touchmove", mousemove)
-    
     renderer.render(scene, camera)
 }
+
+
+const mousemove = (e) => {
+    const client = (e.touches) ? e.touches[0] : e;
+        pointlight.position
+        .setX(
+            (client.clientX - window.innerWidth / 2) /10 * 0.55
+        )
+        .setY(
+            (client.clientY - window.innerHeight / 2) /10 * -.5
+        )
+}
+
+document.addEventListener("mousemove", mousemove)
+document.addEventListener("touchmove", mousemove)
+
+document.addEventListener("scroll", () => {
+    scrollPosition = window.scrollY / document.body.clientHeight
+    if (model) {
+        model.position.y = 5 + (scrollPosition * 20)
+        model.rotation.y = scrollPosition
+        model.rotation.x = Math.PI / 3 - (scrollPosition/2)
+        dirLight.intensity = 9 - (scrollPosition * 17)
+    }
+})
 
  interaction()
